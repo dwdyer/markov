2c96343b5fd5065dbc01edd624509f2255e4047f
dwdyer <dan@uncommons.org>
2013-02-18 19:54:52 +0000

    Read and process multiple separate short pieces of text.  Update RNG state so that subsequent runs aren't duplicates.

--- Markov.hs
+++ Markov.hs
@@ -1,19 +1,22 @@
 module Markov(markov) where
 
 import Data.Map(Map)
-import qualified Data.Map as Map(alter, empty, lookup)
-import Data.Sequence(Seq, (|>))
+import qualified Data.Map as Map(alter, empty, lookup, unionsWith)
+import Data.Sequence(Seq, (|>), (><))
 import qualified Data.Sequence as Seq(index, length, singleton)
 import System.Random(RandomGen, randomR)
 
 type ChainRules a = Map [a] (Seq a)
 
--- | Take a list of values (the input data) and generate Markov mappings of order n, where n is the number
+-- | Take a list of values (the input data) and generate Markov mappings of order n, where n is the number of
 --   previous values to consider when determining the next value.  The map key is a list (of length <= n) of values
 --   and the entry is a sequence of valid next values (may contain duplicates for values that are more likely to
 --   occur than others).
-deriveRules :: Ord a => [a] -> Int -> ChainRules a
-deriveRules input n = buildRules input n $ initRules $ take n input
+deriveRules :: Ord a => Int -> [a] -> ChainRules a
+deriveRules n input = buildRules n input $ initRules $ take n input
+
+deriveRulesMany :: Ord a => Int -> [[a]] -> ChainRules a
+deriveRulesMany n inputs = Map.unionsWith (><) $ map (deriveRules n) inputs
 
 -- | As well has having rules for each sequence of n values, we need a few other rules for shorter sequences
 --   (with lengths from 0 to n-1) at the start so that we can start building the output from an empty list.
@@ -23,9 +26,9 @@ initRules input = updateRule start (last input) $ initRules start
                   where start = init input
 
 -- | This builds the other rules, those that are derived from a full sequence of n previous values.
-buildRules :: Ord a => [a] -> Int -> ChainRules a ->  ChainRules a
-buildRules input n rules
-    | length input > n = buildRules (tail input) n $ updateRule (take n input) (input !! n) rules
+buildRules :: Ord a => Int -> [a] -> ChainRules a ->  ChainRules a
+buildRules n input rules
+    | length input > n = buildRules n (tail input) $ updateRule (take n input) (input !! n) rules
     | otherwise        = rules
 
 -- | Creates a new rule for a given context or updates the existing rule by adding to the sequence of valid
@@ -37,7 +40,6 @@ updateRule context value = Map.alter (addToSequence value) context
 addToSequence :: Ord a => a -> Maybe (Seq a) -> Maybe (Seq a)
 addToSequence value Nothing       = Just $ Seq.singleton value
 addToSequence value (Just values) = Just $ values |> value
-
 
 -- | The next value is determined by the most recent n values (the context) and is randomly selected from the sequence
 --   of valid alternatives in the Markov mappings.
@@ -54,6 +56,6 @@ nextValue output rules rng n = case Map.lookup context rules of
                                where context = drop (length output - n) output
 
 -- | Build a Markov Chain of order n from the input sequence.
-markov :: (RandomGen r, Ord a) => [a] -> r -> Int -> [a]
-markov input rng n = fst $ buildOutput [] rules rng n
-                     where rules = deriveRules input n
+markov :: (RandomGen r, Ord a) => [[a]] -> r -> Int -> ([a], r)
+markov input rng n = buildOutput [] rules rng n
+                     where rules = deriveRulesMany n input
--- Parody.hs
+++ Parody.hs
@@ -2,15 +2,18 @@ module Parody where
 
 import Markov(markov)
 import System.Environment(getArgs)
-import System.Random(RandomGen, getStdGen)
+import System.Random(RandomGen, getStdGen, setStdGen)
 
 -- | The main function expects two arguments, the path of an input text file and the number of previous words to
---   consider when constructing the chain.
+--   consider when constructing the chain.  Each line of the text file is treated as a separate piece of text.
 main :: IO ()
 main = do (inputPath:n:_) <- getArgs
           input <- readFile inputPath
           rng <- getStdGen
-          print $ parody input rng $ read n
+          let (output, rng') = parody (lines input) rng $ read n
+          setStdGen rng'
+          print output
 
-parody :: (RandomGen r) => String -> r -> Int -> String
-parody input rng n = unwords $ markov (words input) rng n
+parody :: (RandomGen r) => [String] -> r -> Int -> (String, r)
+parody input rng n = (unwords output, rng')
+                     where (output, rng') = markov (map words input) rng n
